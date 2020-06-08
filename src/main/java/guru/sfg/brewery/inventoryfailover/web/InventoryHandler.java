9b197f1bf496ef7efc35438848c70752c094cde6
John Thompson <john@springframework.guru>
2020-06-08 08:35:14 -0400

    fixing bug, adding digitalocean profile

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -16,17 +18,15 @@ import java.util.UUID;
 @Component
 public class InventoryHandler {
     public Mono<ServerResponse> listInventory(ServerRequest request){
-
-        BeerInventoryDto.builder().build();
-
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
-                        .id(UUID.randomUUID())
-                        .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
-                        .quantityOnHand(999)
-                        .createdDate(OffsetDateTime.now())
-                        .lastModifiedDate(OffsetDateTime.now())
-                        .build()), BeerInventoryDto.class);
+                .body(Mono.just(Arrays.asList(
+                        BeerInventoryDto.builder()
+                                .id(UUID.randomUUID())
+                                .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                                .quantityOnHand(999)
+                                .createdDate(OffsetDateTime.now())
+                                .lastModifiedDate(OffsetDateTime.now())
+                                .build())), List.class);
     }
 }
